8afaf8fa86bcadd83e74b06fe9918912016ff918
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2023-02-16 12:28:41 +0100

    Initial commit

---
+++ src/main/java/de/lubowiecki/petregisterclient/PetregisterClientApplication.java
@@ -0,0 +1,13 @@
+package de.lubowiecki.petregisterclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetregisterClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetregisterClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/lubowiecki/petregisterclient/PetregisterClientApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lubowiecki.petregisterclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetregisterClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
